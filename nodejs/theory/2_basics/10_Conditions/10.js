8e6197502fe6ba3989d31d7c38513494b97f465f
Andrey Polnikov <polnikov.andrey@gmail.com>
2020-03-23 21:35:36 +0300

    Lesson 10

--- nodejs/theory/2_basics/10_Conditions/10.js
+++ nodejs/theory/2_basics/10_Conditions/10.js
@@ -6,13 +6,28 @@ if (true) {
 } else {
 }
 
-// 0, '', null, undefined, NaN - are treated as false in condition
+// 0, '', null, undefined, NaN - are treated as false in conditions. The are called 'falsy values', all the rest values are called 'truthy'.
 if (1 / 'a') { }      // NaN = false
 
 if (1) {    // Always true
 }
 
-(true) ? console.log(true) : console.log(false);
+const someValue = (4 / 2 == 2) ? 'true' : 'false';
+(someValue === 'true') ? console.log(true) : console.log(false);        // No assignment here, bad practice. It's recommended to use if condition here.
+
+// Here comes if condition usage.
+const age = 10;
+let greeting;
+if (age < 10) {
+    greeting = 'Hello, child!';
+} else if (age < 18) {
+    greeting = 'Hello, young man!';
+} else {
+    greeting = "Good day!";
+}
+// Here comes ternary operator usage with the same meaning and result
+greeting = age < 10 ? 'Hello, child!' : age < 18 ? 'Hello, young man!' : 'Good day!';
+
 
 const aNumber = 1;
 switch (aNumber) {

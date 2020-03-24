12086535c17f6d7abcabe406a516ead442075946
Andrey Polnikov <polnikov.andrey@gmail.com>
2020-03-24 21:51:08 +0300

    Lesson 11

--- nodejs/theory/2_basics/11_Cycles/11.js
+++ nodejs/theory/2_basics/11_Cycles/11.js
@@ -4,6 +4,10 @@ let num = 1;
 while (num < 5) {
     console.log(num++);
 }
+let i = 5;
+while (i) {         // Variable values are converted to boolean using common js rules. Cycle stops when i becomes 0.
+    i--;
+}
 
 console.log('--- do while ---');
 num = 1;
@@ -19,4 +23,27 @@ for (let num = 1; num < 5; num++) {
         break;
     }
     console.log(num);
+}
+
+let j = 0;
+for (; j < 3; j++) {            // The first part of for expression is skipped (variable is initialized already).
+    // Do something
+}
+
+for (let k = 0; k < 3;) {       // The third part of for expression is skipped (variable is changed inside).
+    k++;
+}
+
+// for (;;) {                   // Absolutely legal eternal cycle.
+// }
+
+// Label could be used with cycles to break or continue to the cycle, specified by label.
+labelName: for (let i = 0; i < 3; i++) {    // Break and continue with label should be inside cycle, the label itself - before some cycle and before the break and continue line.
+    for (let j = 0; j < 3; j++) {
+        if (true) {
+            break labelName;
+        } else {
+            continue labelName;
+        }
+    }
 }

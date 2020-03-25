30ba1628424347795056746f942e2edaeb35e67c
Andrey Polnikov <polnikov.andrey@gmail.com>
2020-03-25 23:42:14 +0300

    Lesson 5

--- nodejs/theory/2_basics/5_Js_data_types/5_Data_types.js
+++ nodejs/theory/2_basics/5_Js_data_types/5_Data_types.js
@@ -10,6 +10,14 @@ var boolean = true;             // boolean - true/false.
 var aNull = null;
 var anUndefined = undefined;    // jshint ignore: line
 
+console.log(typeof number);         // Output: number
+console.log(typeof string);         // Output: string
+console.log(typeof symbol);         // Output: symbol
+console.log(typeof boolean);        // Output: boolean
+console.log(typeof aNull);          // Output: object           This is a known js bug, null is an object type.
+console.log(typeof anUndefined);    // Output: undefined
+
+
 /* ----- There are 6 object data types. ----- */
 var object = {};    // Simple object.
 var array = [];     // Array.
@@ -17,6 +25,19 @@ var aFunction = function() {};
 var date = new Date();
 var regexp = new RegExp('');
 var error = new Error();
+
+console.log(typeof object);         // Output: object
+console.log(typeof array);          // Output: object
+console.log(typeof aFunction);      // Output: function         Function is also an object, but typeof returns 'function'.
+console.log(typeof date);           // Output: object
+console.log(typeof regexp);         // Output: object
+console.log(typeof error);          // Output: object
+
+/* ----- bigint ----- */
+// There is additional numeric data type bigint - to deal with integer values of arbitrary length.
+// const aBigInt1 = BigInt(12312312312);
+// const aBigInt2 = 12312123n;              // Note: number ends with 'n' - means bigint;
+// console.log(aBigInt);                    // Output: bigint
 
 
 /* ----- Number type variable could contain: Integer, Float, Infinity and NaN values. ----- */
